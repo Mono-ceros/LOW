6d0e28685c615aa605cc30ce02781c408827bc05
Seung-Do <gablin21@hotmail.com>
2023-07-12 17:45:51 +0900

    v0.27
    motion sheild spawn

---
+++ Assets/Scripts/Recognizer.cs
@@ -0,0 +1,113 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+using PDollarGestureRecognizer;
+using System.IO;
+using UnityEngine.Events;
+
+public class Recognizer : MonoBehaviour
+{
+    public XRNode inputSource;
+    public InputHelpers.Button inputButton;
+    public Transform movementSource;
+    private float inputThreshold = 0.1f;
+    private bool isMoving = false;
+    private float distanceThreshold = 0.05f;
+
+    public GameObject debugCubePrefab;
+    public bool creationMode;
+    public string newGestureName;
+
+    private float recognitionThreshold = 0.8f;
+
+    [System.Serializable]
+    public class UnityStringEvent : UnityEvent<string> { }
+    public UnityStringEvent OnRecognized;
+
+    private List<Gesture> trainingSet = new List<Gesture>();
+    private List<Vector3> positionsList = new List<Vector3>();
+    
+
+
+    void Start()
+    {
+        string[] gestureFiles = Directory.GetFiles(Application.persistentDataPath, "*.xml");
+        foreach(var item in gestureFiles)
+        {
+            trainingSet.Add(GestureIO.ReadGestureFromFile(item));   
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InputHelpers.IsPressed(InputDevices.GetDeviceAtXRNode(inputSource), inputButton, out bool isPressed, inputThreshold);
+        //모션 시작
+        if (!isMoving && isPressed)
+        {
+            StartMovement();
+        }
+        //모션 종료
+        else if (isMoving && !isPressed)
+        {
+            EndMovement();
+        }
+        //모션 중일 때
+        else if (isMoving && isPressed)
+        {
+            UpdateMovement();
+        }
+    }
+
+    void StartMovement()
+    {
+        //Debug.Log("스타트");
+        isMoving = true;
+        positionsList.Clear();
+        positionsList.Add(movementSource.position);
+        if (debugCubePrefab)
+            Destroy(Instantiate(debugCubePrefab, movementSource.position, Quaternion.identity),2f);
+    }
+    void EndMovement()
+    {
+        //Debug.Log("종료");
+        isMoving = false;
+        Point[] pointArray = new Point[positionsList.Count];
+        for(int i = 0; i < positionsList.Count; i++)
+        {
+            Vector2 screenPoint = Camera.main.WorldToScreenPoint(positionsList[i]);
+            pointArray[i] = new Point(screenPoint.x, screenPoint.y, 0);
+        }
+        Gesture newGesture = new Gesture(pointArray);
+        if(creationMode)
+        {
+            newGesture.Name = newGestureName; 
+            trainingSet.Add(newGesture);
+            string fileName = Application.persistentDataPath + "/" + newGestureName + ".xml";
+            GestureIO.WriteGesture(pointArray,newGestureName,fileName);
+        }
+        else
+        {
+            Result result = PointCloudRecognizer.Classify(newGesture, trainingSet.ToArray());    
+            //Debug.Log(result.GestureClass +  result.Score);
+            if(result.Score > recognitionThreshold)
+            {
+                OnRecognized.Invoke(result.GestureClass);
+            }
+        }
+    }
+    void UpdateMovement()
+    {
+        //Debug.Log("움직이는 중");
+        Vector3 lastPosition = positionsList[positionsList.Count - 1];
+        if (Vector3.Distance(movementSource.position, lastPosition) > distanceThreshold)
+        {
+            positionsList.Add(movementSource.position);
+            if (debugCubePrefab)
+                Destroy(Instantiate(debugCubePrefab, movementSource.position, Quaternion.identity), 2f);
+        }
+    }
+       
+}
